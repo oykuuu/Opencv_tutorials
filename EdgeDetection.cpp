bdbcfd70994dde552f27e3d2ba1e6b5288ea118f
Oyku <han.oyku@gmail.com>
2019-11-17 16:20:01 -0500

    Fixed edge detection

--- EdgeDetection.cpp
+++ EdgeDetection.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <opencv2/core/core.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
@@ -9,32 +10,40 @@ using namespace cv;
 
 void show_image(Mat &img)
 {
+    /* Shows image
+    img
+       cv::Mat, input image
+    */
     string name = "image";
     namedWindow(name);
     imshow(name, img);
     waitKey();
 }
 
-Mat convolve(Mat &img, Mat &sobel_value)
+Mat sobel_filter(Mat &img, Mat &sobel_x, Mat &sobel_y)
 {
-    Mat sobel(3, 3, CV_8UC1, &sobel_value);
-    Mat output(img.size(), 3, CV_8UC1);
+    /* Convolution with Sobel filter to detect edges
+    img
+        cv::Mat, input image
+    sobel_x
+        cv::Mat, sobel filter for derivate along x direction
+    sobel_y
+        cv::Mat, sobel filter for derivate along y direction
+    */
+    Mat sobel(3, 3, CV_32FC1, &sobel_x);
+    Mat output(img.size(), CV_32FC1);
     Mat padded;
+    Mat rect_img;
+    float val_x, val_y;
     copyMakeBorder(img, padded, 1, 1, 1, 1, BORDER_REPLICATE);
     for(int i = 1; i < img.rows-1; ++i)
     {
-        //uchar* padded_row_ptr = padded.ptr(i+1);
         for(int j = 1; j < img.cols-1; ++j)
         {
-            // uchar col_ptr = row_ptr[j+1];
-            Mat rect_img = padded(Rect(j-1, i-1, 3, 3));
-            float val = rect_img.dot(sobel_value)/255;
-            //cout << val << endl;
-            if (val < 0)
-                val = 0;
-            if (val > 255)
-                val = 255;
-            output.at<float>(i,j) = val;
+            rect_img = padded(Rect(j-1, i-1, 3, 3));
+            val_x = rect_img.dot(sobel_x);
+            val_y = rect_img.dot(sobel_y);
+            output.at<float>(i,j) = sqrt(pow(val_x, 2) + pow(val_y, 2));
         }
     }
     return output;
@@ -42,25 +51,21 @@ Mat convolve(Mat &img, Mat &sobel_value)
 
 int main()
 {
-    Mat image, image_color;
-    image_color = imread("images/lenna.jpg", IMREAD_COLOR);
-    //cvtColor(image_color, image_color, COLOR_BGR2GRAY);
+    Mat image;
+    image = imread("images/lenna.jpg", IMREAD_GRAYSCALE);
+    image.convertTo(image, CV_32FC1);
 
-    //double minVal, maxVal;
-    //minMaxLoc(image_color, &minVal, &maxVal);
-    //image_color.convertTo(image, CV_8U, 255.0/(maxVal - minVal), -minVal * 255.0/(maxVal - minVal));
-    
-    cout << image.size() << endl;
-    cout << image.at<float>(149, 123) << endl;
+    float sobel_val[3][3] = {{1, 0, -1},{2, 0, -2},{1, 0, -1}};
+    Mat filter_x(3,3,CV_32FC1, &sobel_val);
+    Mat filter_y;
+    transpose(filter_x, filter_y);
 
+    Mat output = sobel_filter(image, filter_x, filter_y);
+    image.convertTo(image, CV_8UC1);
+    output.convertTo(output, CV_8UC1);
+    hconcat(image, output, output);
 
-
-    show_image(image);
-    float sobel_val[3][3] = {{1, 0, -1},{2, 0, -2},{1, 0, -1}}; //Iy * Ix
-    Mat filter(3,3,CV_8UC1, &sobel_val);
-    Mat output = convolve(image, filter);
     show_image(output);
-    cout << output.size() << endl;
 
     return 0;
 }
